46e17aef82b830e8497ba37a0e1ee2d71532c698
exdead01 <40805891+exdead01@users.noreply.github.com>
2020-04-29 02:10:32 -0400

    Added backup json, started matching algorithm

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const client = new Discord.Client();
+const jsonWriteTime = (2*60)*1000; //Minutes * seconds in a minute * milliseconds in a second
 
 const TOKEN = process.env.BOT_TOKEN;
 
@@ -8,9 +9,9 @@ var User_History =[];
 
 // Constructors
 
-function User(tag)
+function User(id)
 {
-    this.tag = tag;
+    this.id = id;
     this.dict = [];
 }
 function Word(word)
@@ -36,48 +37,76 @@ client.on('message', msg=>{
         words = msg.content.split(" ");
         if(words[0] == "!matchme")
         {
-            var inArray = isInArray(msg.author.tag);
-            if(!inArray[0])
-            {
-                msg.channel.send("no u");
-            }
-            else
-            {
-                msg.channel.send('No matches, ' + User_History[inArray[1]].tag);
-            }
+            msg.channel.send('Please Wait, @<' + msg.author.id + '>');
+            msg.channel.send(matchMe(msg.author.id));
         }
+        // else if(words[0] == "!readme")
+        // {
+        //     var inArray = isInArray(msg.author.id);
+        //     if(!inArray[0])
+        //     {
+        //         msg.channel.send("no u");
+        //     }
+        //     else
+        //     {
+        //         for(var i=0;i<User_History[inArray[1]].dict.length;i++)
+        //         {
+        //             msg.channel.send(User_History[inArray[1]].dict[i]);
+        //         }
+        //     }
+        // }
         else
         {
             for(var i=0; i<words.length; i++)
             {
-                var inArray = isInArray(msg.author.tag);
+                console.log('Word: ' + words[i]);
+                var inArray = isInArray(msg.author.id);
                 if(!inArray[0])
                 {
-                    var newUser = new User(msg.author.tag);
-                    console.log('Adding new user: '+ newUser.tag);
+                    var newUser = new User(msg.author.id);
+                    console.log('Adding new user: '+ newUser.id);
                     newUser.dict.push(words[i])
                     User_History.push(newUser)
                 }
                 else
                 {
-                    //This needs to be more complicated.
-                    //User_History[inArray[1]].dict.push(words[i]);
-                    console.log("Adding word!");
+                    console.log("Changing/Adding word!");
+                    for(var j=0; j<User_History[inArray[1]].dict.length; j++)
+                    {
+                        console.log(User_History[inArray[1]].dict[j] + ": :" + words[i]);
+                        console.log(User_History[inArray[1]].dict.length + ": :" + words.length);
+
+                        if(User_History[inArray[1]].dict[j] == words[i])
+                        {
+                            //if the word alreasy exists
+                            User_History[inArray[1]].dict[j].times++;
+                            console.log("Increase words number");
+                            break;
+                        }
+                        else if(j==User_History[inArray[1]].dict.length-1)
+                        {
+                            //if the word doesnt exist and we're also at the end
+                            User_History[inArray[1]].dict.push(words[i]);
+                            console.log("!!!!add word!!!!!");
+                            break;
+                        }
+                    }
                 }
             }
         }
     }
-    console.log(msg.author.tag + ": " + msg.content);
+    console.log(msg.author.id + ": " + msg.content);
     //for(var i=0; i<User_History.length; i++)
     //{
     //    console.log(User_History[i].name);
     //}
-    console.log('Array length: '+ User_History.length);
+    //console.log('User Array length: '+ User_History.length);
+    //console.log('Word Array length: '+ User_History[0].dict.length);
 })
 
-function isInArray(tag)
+function isInArray(id)
 {
-    if(typeof(tag) != "string")
+    if(typeof(id) != "string")
     {
 
         return [0,0];
@@ -86,11 +115,20 @@ function isInArray(tag)
     {
         for(var i=0; i<User_History.length; i++)
         {
-            if(User_History[i].tag == tag)
+            if(User_History[i].id == id)
             {
                 return [1,i];
             }
         }
         return [0,0];
     }
+}
+
+function matchMe(id)
+{
+    if(isInArray(id)==0)
+    {
+        return "No matches yet, " + id;
+    }
+    return "Error: End of matchMe(id)"
 }
